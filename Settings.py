099e2adc675418cd4add3765a0a8e35ab552e196
Nachiket Patel <patelnachiket828@gmail.com>
2023-04-11 22:24:09 +0530

    Error solved

---
+++ Settings.py
@@ -0,0 +1,9 @@
+class settings:
+    """A class to store all settings for alien invasion"""
+
+    def __init__(self):
+        """Initialize the game settings"""
+        # Screen settings
+        self.scree_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
---
+++ alien_invesion.py
@@ -0,0 +1,34 @@
+import sys
+import pygame
+from Settings import settings
+import self
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour"""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
+
+
+def run_game():
+    """Start the main loop for the game."""
+
+    while True:
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+        if __name__ == '__main__':
+            ai = AlienInvasion()
+            ai.run_game()
